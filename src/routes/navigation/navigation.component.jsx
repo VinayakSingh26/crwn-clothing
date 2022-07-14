7c4af16e5f6b171cd42db8cb38b5ecc59e5a9352
VinayakSingh26 <iamvinayaksingh@gmail.com>
2022-07-14 22:55:31 +0530

    adding redirect

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -10,7 +10,7 @@ import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
-import './navigation.styles.scss';
+import './navigation.styles.jsx';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
